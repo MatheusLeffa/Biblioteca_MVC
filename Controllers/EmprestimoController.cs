2b90961aa41df49de5d452389f4f56d9c98eed5a
Matt_Leffa <83082324+MatheusLeffa@users.noreply.github.com>
2024-06-18 03:41:58 -0300

    Implementação de solução web MVC

---
+++ Controllers/EmprestimoController.cs
@@ -0,0 +1,98 @@
+namespace Biblioteca_MVC.Controllers;
+
+using Biblioteca_MVC.Data;
+using Biblioteca_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class EmprestimoController : Controller
+{
+    private readonly ApplicationDbContext _dataBase;
+
+    public EmprestimoController(ApplicationDbContext dataBase)
+    {
+        _dataBase = dataBase;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        IEnumerable<EmprestimosModel> emprestimos = _dataBase.Emprestimos;
+        return View(emprestimos);
+    }
+
+    [HttpGet]
+    public IActionResult Cadastrar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Cadastrar(EmprestimosModel emprestimos)
+    {
+        if (ModelState.IsValid)
+        {
+            _dataBase.Emprestimos.Add(emprestimos);
+            _dataBase.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Editar(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        EmprestimosModel? emprestimo = _dataBase.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+        if (emprestimo == null)
+        {
+            return NotFound();
+        }
+
+        return View(emprestimo);
+    }
+
+    [HttpPost]
+    public IActionResult Editar(EmprestimosModel emprestimo)
+    {
+        if (ModelState.IsValid)
+        {
+            _dataBase.Emprestimos.Update(emprestimo);
+            _dataBase.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(emprestimo);
+    }
+
+    [HttpGet]
+    public IActionResult Excluir(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        EmprestimosModel? emprestimo = _dataBase.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+        if (emprestimo == null)
+        {
+            return NotFound();
+        }
+
+        return View(emprestimo);
+    }
+
+    [HttpPost]
+    public IActionResult Excluir(EmprestimosModel emprestimo)
+    {
+        if (emprestimo == null)
+        {
+            return NotFound();
+        }
+        _dataBase.Emprestimos.Remove(emprestimo);
+        _dataBase.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
---
+++ Models/EmprestimosModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca_MVC.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informado o nome do Recebedor!")]
+        public string Recebedor { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informado o nome do Fornecedor!")]
+        public string Fornecedor { get; set; }
+
+        [Required(ErrorMessage = "Deve ser informado o nome do Livro!")]
+        public string LivroEmprestado { get; set; }
+
+        public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
+    }
+}
